d2623ced42128af4f24f901774c6dc7f29e45817
chloecs <13693540615@163.com>
2018-08-17 11:01:11 +0800

    ca 上传页面

--- cca_portal/WebContent/app/OperationData/controllers/CAmanualuploadCtrl.js
+++ cca_portal/WebContent/app/OperationData/controllers/CAmanualuploadCtrl.js
@@ -1,29 +1,31 @@
 "use strict";
 
 angular.module('app.OperationData').controller('CAmanualuploadCtrl', function ($scope,$state,$stateParams,$location) {
-//button 切换
-    $scope.ww = true;
-    $scope.sw1 = false;
+    //button 切换
+    $scope.sw1 = true;
+    $scope.ww = false;
     $scope.sw2 = false;
-    $scope.btnSwitch = function(flag){
-        if(flag == 'w'){
-            $scope.ww = false;
-            $scope.sw1 = true;
-            $scope.sw2 = false;
-        }else if(flag == 'p'){
+    $scope.btnSwitch = function (flag) {
+        if (flag == 'w') {
             $scope.ww = true;
             $scope.sw1 = false;
-            $scope.sw2 = false;
+            //$scope.sw2 = false;
+        } else if (flag == 'p') {
+            $scope.ww = false;
+            $scope.sw1 = true;
+            //$scope.sw2 = false;
         }
     }
 
-    $scope.btnSV = function(flag){
-        if(flag == 's1'){
+    $scope.btnSV = function (flag) {
+        if (flag == 's1') {
             $scope.sw2 = true;
             $scope.sw1 = false;
-        }else if(flag == 's2') {
+            $scope.ww=false;
+        } else if (flag == 's2') {
             $scope.sw1 = true;
             $scope.sw2 = false;
+            $scope.ww=false;
         }
     }
 
