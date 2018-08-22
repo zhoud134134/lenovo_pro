b0036d0395d6bd5f97b5abec4fbb240ad0314db4
HYN7656 <40452138+HYN7656@users.noreply.github.com>
2018-08-22 10:05:04 +0800

    冲突提交

---
+++ cca_portal/WebContent/app/layout/service.js
@@ -0,0 +1,22 @@
+angular.module('app.layout').service("navService", function($http, $q , APP_CONFIG) {
+
+    //第一部分Select中第二第三个框
+    this.getUser = function(type) {
+        var d = $q.defer();
+        $http({
+            method : 'GET',
+            //http://10.99.123.10:8080/lenovo-ccf-prod/api/bmc/
+            url : APP_CONFIG.baseUrl +'/adfs/user',
+        }).then(function successCallback(response) {
+            // 请求成功执行代码
+            d.resolve(response.data);
+        }, function errorCallback(response) {
+            // 请求失败执行代码
+            d.reject("error");
+        });
+        return d.promise;
+    }
+
+
+
+});
