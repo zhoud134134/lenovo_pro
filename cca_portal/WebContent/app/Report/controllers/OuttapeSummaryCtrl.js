ced93ba7a270f00863aa660b876ece7e2d2f16ef
yangxiaolong405 <yxl30891094>
2018-09-05 17:37:28 +0800

    other三维表更改

--- cca_portal/WebContent/app/Report/controllers/OuttapeSummaryCtrl.js
+++ cca_portal/WebContent/app/Report/controllers/OuttapeSummaryCtrl.js
@@ -1,27 +1,6 @@
 "use strict";
 
 angular.module('app.Report').controller('OuttapeSummaryCtrl', function ($scope,$state,$stateParams,$timeout) {
-    //datatable ±íÍ·¹Ì¶¨
-    $scope.reporttab=function(){
-        $("#tabExample1").dataTable().fnDestroy();
-        $timeout(function () {
-            $('#tabExample1').dataTable({
-                "scrollY":260,
-                "scrollX": true,
-                "dom": '<"top">rt<"bottom"><"clear">',
-                "scrollCollapse": true,
-                "jQueryUI": true,
-                // "pagingType":   "simple_numbers",
-                stateSave: true,
-                "paging": false,
-                "ordering": false,
-                "bLengthChange": true,
-                //"order": [[ 3, "desc" ]],
-                "fixedColumns" :{
-                    leftColumns: 2,
-                }
-            });
-        })
-    }
-    $scope.reporttab();
+    $('#final table').stickySort({ sortable: true });
+
 })
