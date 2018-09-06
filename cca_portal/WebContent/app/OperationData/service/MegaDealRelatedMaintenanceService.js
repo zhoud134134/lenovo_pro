70df1640d63b7837facd0977e201b996f49a547d
HYN7656 <40452138+HYN7656@users.noreply.github.com>
2018-09-06 16:52:44 +0800

    11

---
+++ cca_portal/WebContent/app/OperationData/service/MegaDealRelatedMaintenanceService.js
@@ -0,0 +1,63 @@
+angular.module('app.OperationData').service("MegaDealRelatedMaintenance", function($http, $q , APP_CONFIG) {
+
+    //获取数据
+    this.getData = function(cycle) {
+        console.log(cycle)
+        var d = $q.defer();
+        $http({
+            method : 'GET',
+            //http://10.99.123.10:8080/lenovo-ccf-prod/api/bmc/
+            url : APP_CONFIG.baseUrl +'/api/mega/'+cycle,
+            headers: {
+                'Authorization': 'Bearer '+ sessionStorage.getItem("token")
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for (var s in obj) {
+                    str.push(encodeURIComponent(s) + "=" + encodeURIComponent(obj[s]));
+                }
+                return str.join("&");
+            },
+            //params : cycle
+        }).then(function successCallback(response) {
+            // 请求成功执行代码
+            d.resolve(response.data);
+        }, function errorCallback(response) {
+            // 请求失败执行代码
+            d.reject("error");
+        });
+        return d.promise;
+    }
+
+    /**
+     * 下载模板
+     */
+    this.download = function (load) {
+        console.log(load)
+        var d = $q.defer();
+        $http({
+            method: 'GET',
+            url: APP_CONFIG.baseUrl + '/api/loadfile/loadexcel',
+            transformRequest: function (obj) {
+                var str = [];
+                for (var s in obj) {
+                    str.push(encodeURIComponent(s) + "=" + encodeURIComponent(obj[s]));
+                }
+                return str.join("&");
+            },
+            headers: {
+                'Authorization': 'Bearer '+ sessionStorage.getItem("token")
+            },
+            params: load,
+            responseType: 'arraybuffer'
+        }).then(function successCallback(response) {
+            // 请求成功执行代码
+            d.resolve(response.data);
+        }, function errorCallback(response) {
+            // 请求失败执行代码
+            d.reject("error");
+        });
+        return d.promise;
+    }
+
+});
