e15fa9c8bdd025926d43bf8ecd7299c0f98f9d24
Qinglanhui <13693540615@163.com>
2018-09-19 14:40:24 +0800

    Account改

--- cca_portal/WebContent/app/OperationData/controllers/AccountTemplateManualUploadCtrl.js
+++ cca_portal/WebContent/app/OperationData/controllers/AccountTemplateManualUploadCtrl.js
@@ -1,176 +1,81 @@
 "use strict";
 
-angular.module('app.OperationData').controller('AccountTemplateManualUploadCtrl', function ($scope,MegaDealRelatedMaintenance,AccountTemplateManualUploadService,CAmaintenanceService,$timeout,$rootScope,Upload,APP_CONFIG,$state,$stateParams,$location) {
-	 $rootScope.getCycle('Actual').then(function(data){
-	        $scope.cycledata = data.result;
-	    });
-    $('#final table').stickySort({ sortable: true });
+angular.module('app.OperationData').controller('AccountTemplateManualUploadCtrl', function ($scope, OthercategorymaintenanceService, AccountTemplateManualUploadService, CAmaintenanceService, $timeout, $rootScope, Upload, APP_CONFIG, $state, $stateParams, $location) {
+    $rootScope.getCycle().then(function (data) {
+        $scope.cycledata = data.result;
+    });
+    $('#final table').stickySort({sortable: true});
+
     //上传
     $scope.myfiles = {};
     $scope.myfilesVal = '';
-    $scope.fileChange = function(){
-        if($scope.myfiles.name){
+    $scope.fileChange = function () {
+        if ($scope.myfiles.name) {
             $scope.myfilesVal = $scope.myfiles.name;
-        }else {
+        } else {
             $scope.myfilesVal = '';
         }
     }
 
-    $scope.ww=false;
-    $scope.upload = function(){
-        Upload.upload({
-            //服务端接收
-            url:APP_CONFIG.baseUrl+ '/api/dm/ca/attachments',
-            data : {
-                file : $scope.myfiles,
-                username :$rootScope.user,
-                cyclename:$scope.CycleChoose
-            },
-            headers: {
-                'Authorization': 'Bearer '+ sessionStorage.getItem("token")
-            },
-        }).success(function (data, status, headers, config){
-            //console.log($scope.CycleChoose.indexOf("M0")==-1);
-            if(!$scope.CycleChoose){
-                alert("请选择条件！");
-            }else {
-                if (data.code == 0 && $scope.CycleChoose.indexOf("M0") != -1) {
-                    alert('Success');
-                    $scope.ww=true;
+    $scope.account = false;
+    $scope.upload = function () {
+        if (!$scope.CycleChoose) {
+            alert("请选择条件！");
+        } else {
+            $('#upload1').css('display','none');
+            $('#upload2').css('display','block');
+            Upload.upload({
+                //服务端接收
+                url: APP_CONFIG.baseUrl + '/api/SUMACT/',
+                data: {
+                    file: $scope.myfiles,
+                    username: $rootScope.user,
+                    cycle: $scope.CycleChoose
+                },
+                headers: {
+                    'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+                }
+            }).success(function (data, status, headers, config) {
+                console.log(data);
+                if (data.code == 0) {
                     $scope.id=data.result;
-                    console.log($scope.id);
-                    //$('#myModal').modal('hide');
-                    $scope.getPage();
-                }else if(data.code == 0 && $scope.CycleChoose.indexOf("Actual") != -1){
-                    alert('Success');
-                    $scope.ww=true;
-                    $scope.id=data.result;
-                    console.log($scope.id);
-                    //$('#myModal').modal('hide');
-                    $scope.getPage();
+                    console.log(data);
+                    //请求表格数据调用方法
+                    AccountTemplateManualUploadService.getOthercategoryData($scope.CycleChoose).then(function(data){
+                        if(data.code == 0){
+                            $scope.account=true;
+                            console.log(data);
+                            $scope.categoryData = data.result;
+                            var geo = $rootScope.getFiled($scope.categoryData,"geo");
+                            var categorylvl1 = $rootScope.getFiled($scope.categoryData,"categorylvl1");
+                            var categorylvl2 = $rootScope.getFiled($scope.categoryData,"categorylvl2");
+                            var categorylvl3 = $rootScope.getFiled($scope.categoryData,"categorylvl3");
+                            $scope.dataMap = OthercategorymaintenanceService.getDataMap($scope.categoryData,geo,categorylvl1,categorylvl2,categorylvl3);
+
+                            $('#upload1').css('display','block');
+                            $('#upload2').css('display','none');
+                        }
+                    },function(data){
+                        console.log(data);
+                    });
                 } else {
                     alert('Uploading Failed');
                 }
-            }
-        }).error(function (data, status, headers, config) {
-            alert('Uploading Failed');
-            //上传失败
-            console.log('error status: ' + status);
-        });
-    }
-
-    //第二部分tab信息展示
-    $scope.getPage = function(){
-        CAmaintenanceService.getExecute2().then(function(data){
-            if(data.code == 0){
-                $scope.tablist = data.result;
-                $("#tabExample1").dataTable().fnDestroy();
-                $timeout(function () {
-                    $('#tabExample1').dataTable({
-                        "scrollY": 160,
-                        "scrollX": true,
-                        "dom": '<"top">rt<"bottom"><"clear">',
-                        "scrollCollapse": true,
-                        //"jQueryUI": true,
-                        // "pagingType":   "simple_numbers",
-                        stateSave: true,
-                        "paging": false,
-                        "ordering": false,
-                        "bLengthChange": true
-                    });
-                });
-            }
-            console.log(data);
-        },function(data){
-            console.log(data);
-        });
-    }
-    $scope.getPage();
-    //单击整行选中
-    $scope.trClick = function($event,id,status,cycleName){
-        $($("#tabExample input:radio")).removeAttr("checked");
-        $($event.target).parent().find("input:radio").prop("checked",true);
-        $scope.taskId = id;
-        console.log($scope.taskId)
-        $scope.status = status;
-        console.log($scope.status)
-        $scope.cyclename = cycleName;
-        console.log($scope.cyclename)
-        //$scope.SearchTaskId(a,b,c)
-    }
-
-    //点击Search
-    $scope.SearchTab = function(){
-        if(!$scope.taskId){
-            alert("请选择项！");
-        }else if($scope.status =='Success' || $scope.status =='Publish'){
-            $scope.ww=true;
-
-            $scope.TaskID =  $scope.taskId;
-            $scope.CycleName = $scope.cyclename;
-
-            //WW
-            CAmaintenanceService.getWw($scope.TaskID).then(function(data){
-                if(data.code == 0){
-                    $scope.WwList = data.result;
-                    console.log($scope.WwList);
-                }
-                console.log(data);
-            },function(data){
-                console.log(data);
+            }).error(function (data, status, headers, config) {
+                alert('Uploading Failed');
+                //上传失败
+                console.log('error status: ' + status);
             });
-
-            //PRC
-            CAmaintenanceService.getPrc($scope.TaskID).then(function(caprcdata) {
-                console.log(caprcdata);
-                if (caprcdata.code == 0) {
-                    $scope.PrcList = caprcdata.result;
-                    console.log($scope.PrcList);
-                }
-            } ,function(data){
-                console.log(data);
-            });
-        }else {
-            alert("暂未执行成功，无法查看！");
         }
-    };
+    }
 
-    //删除
-    $scope.DelOneItem = function(){
-        if(!$scope.taskId){
-            alert("请选择项！");
-        }else if($scope.status =='Success' || $scope.status =='Publish'|| $scope.status =='Error'){
-            if(confirm('确认要删除？')) {
-                console.log($scope.taskid);
-                $scope.taskid = {
-                    uuid: $scope.taskId
-                };
-                CAmaintenanceService.DelItem($scope.taskid).then(function (data) {
-                    if (data.code == 0) {
-                        alert("删除成功！");
-                        $scope.taskId = '';
-                        $scope.getPage();
-                        //$("#tabExample").dataTable().fnDestroy();
-                        //$scope.PRCWW = true;
-                    }else {
-                        alert(data.msg);
-                    }
-                    console.log(data);
-                }, function (data) {
-                    console.log(data);
-                });
-            }
-        }else {
-            alert("还未执行完成！");
-        }
-    };
 
     //下载模板
     $scope.DowTemp = function(){
         $scope.temp = {
-            type: 'megadeal'
+            type: 'account'
         }
-        MegaDealRelatedMaintenance.download($scope.temp).then(function(data){
+        OthercategorymaintenanceService.download($scope.temp).then(function(data){
             console.log(data);
             var blob = new Blob([data], {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"});
             var objectUrl = URL.createObjectURL(blob);
--- cca_portal/WebContent/app/OperationData/service/AccountTemplateManualUploadService.js
+++ cca_portal/WebContent/app/OperationData/service/AccountTemplateManualUploadService.js
@@ -1,9 +1,56 @@
 /**
  * Created by Qinglanhui on 2018/9/12.
  */
-angular.module('app.OperationData').service("AccountTemplateManualUploadService", function($http, $q ,$rootScope, APP_CONFIG) {
+angular.module('app.OperationData').service("AccountTemplateManualUploadService", function ($http, $q, $rootScope, APP_CONFIG) {
+    //获取第二部分表格数据
+    //this.getExecute2 = function () {
+    //    var d = $q.defer();
+    //    $http({
+    //        method: 'GET',
+    //        url: APP_CONFIG.baseUrl + '/api/CAMaintenanceBmc/',
+    //        headers: {
+    //            'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+    //        },
+    //    }).then(function successCallback(response) {
+    //        // 请求成功执行代码
+    //        d.resolve(response.data);
+    //    }, function errorCallback(response) {
+    //        // 请求失败执行代码
+    //        d.reject("error");
+    //    });
+    //    return d.promise;
+    //}
+    //
+    ////删除第二部分某一项
+    //this.DelItem = function (id) {
+    //    //  console.log(id)
+    //    var d = $q.defer();
+    //    $http({
+    //        method: 'DELETE',
+    //        url: APP_CONFIG.baseUrl + '/api/caMaintenanceUUid/',
+    //        /*transformRequest: function(obj) {
+    //         var str = [];
+    //         for (var s in obj) {
+    //         str.push(encodeURIComponent(s) + "=" + encodeURIComponent(obj[s]));
+    //         }
+    //         return str.join("&");
+    //         },*/
+    //        params: id,
+    //        headers: {
+    //            'Authorization': 'Bearer ' + sessionStorage.getItem("token"),
+    //            'Content-Type': 'application/x-www-form-urlencoded'
+    //        },
+    //    }).then(function successCallback(response) {
+    //        // 请求成功执行代码
+    //        d.resolve(response.data);
+    //    }, function errorCallback(response) {
+    //        // 请求失败执行代码
+    //        d.reject("error");
+    //    });
+    //    return d.promise;
+    //}
     /**
-     * 最下边的下载
+     * 下载模板
      */
     this.download = function (load) {
         console.log(load)
@@ -33,4 +80,22 @@ angular.module('app.OperationData').service("AccountTemplateManualUploadService"
         return d.promise;
     }
 
+    //请求AccountTemplateManualUpload表格 接口
+    this.getOthercategoryData = function (cycle) {
+        var d = $q.defer();
+        $http({
+            method: 'GET',
+            url: APP_CONFIG.baseUrl + '/api/SUMACT/?cycle=' + cycle,
+            headers: {
+                'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+            },
+        }).then(function successCallback(response) {
+            // 请求成功执行代码
+            d.resolve(response.data);
+        }, function errorCallback(response) {
+            // 请求失败执行代码
+            d.reject("error");
+        });
+        return d.promise;
+    }
 })
