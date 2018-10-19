69cce20dd0572dfac84e07020e365a76f87768fe
zhoudan <zhoudan@lovefamily>
2018-10-19 14:24:41 +0800

    3

--- cca_portal/WebContent/app/OperationData/controllers/OthercategorymaintenanceCtrl.js
+++ cca_portal/WebContent/app/OperationData/controllers/OthercategorymaintenanceCtrl.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module('app.OperationData').controller('OthercategorymaintenanceCtrl', function ($scope,$http,$timeout,OthercategorymaintenanceService,$state,$stateParams,$rootScope,$location,Upload,APP_CONFIG) {
-    $rootScope.getCycle('FCST').then(function(data){
+    $rootScope.getCycle().then(function(data){
         $scope.cycledata = data.result;
     });
     $scope.ww = true;
